ea57aa63d925e9cfa14b0d1ecf668dbb3abea1f3
Manikanta reddy padala <109054645+Manikanta0997@users.noreply.github.com>
2024-04-06 20:37:43 +0530

    Update app.py

--- WEB/app.py
+++ WEB/app.py
@@ -11,10 +11,10 @@ import sklearn
 import numpy as np
 
 st.set_page_config(
-    page_title="Multipage App",
+    page_title="XAI Project",
     page_icon="👋",
 )
-st.title("Main Page")
+st.title("Heart Disease Prediction Integrating With XAI")
 st.sidebar.success("Select a page above.")
 
 st.write('Cardiovascular diseases (CVDs) are the number 1 cause of death globally, taking an estimated 17.9 million lives each year, which accounts for 31% of all deaths worldwide. Four out of 5CVD deaths are due to heart attacks and strokes, and one-third of these deaths occur prematurely in people under 70 years of age. Heart failure is a common event caused by CVDs and this dataset contains 11 features that can be used to predict a possible heart disease.')
